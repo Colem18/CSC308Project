e671dddfc1ad4b4b1670d40111d6d72757767b49
devmasrani <dmasrani@calpoly.edu>
2022-11-23 01:30:15 +0100

    Added Login

---
+++ Data/HttpRestService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+
+namespace eventsApp.Data
+{
+	public class HttpRestService
+	{
+        public HttpClient _client;
+        public JsonSerializerOptions _serializerOptions;
+        public HttpRestService()
+		{
+            _client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+	}
+}
+
