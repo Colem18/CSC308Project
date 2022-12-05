c55bb6dff708a846ac64b86a4b28208470b87334
devmasrani <dmasrani@calpoly.edu>
2022-12-05 23:49:19 +0100

    Added Edit Event

---
+++ Data/EventLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace eventsApp.Data
+{
+    public class EventLoader
+    {
+        HttpRestService _httprestservice = ServiceHelper.GetService<HttpRestService>();
+
+    public async Task<List<Event>> GetEventsAsync()
+        {
+        List<Event> Items = new List<Event>();
+        Uri uri = new Uri(string.Format("https://romaeventfunction.azurewebsites.net/api/event", string.Empty));
+        Console.WriteLine(uri);
+        try
+        {
+            HttpResponseMessage response = await _httprestservice._client.GetAsync(uri);
+            //Console.WriteLine(response.IsSuccessStatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+                //Items = JsonSerializer.Deserialize(content,_httprestservice._serializerOptions);
+                Items = JsonConvert.DeserializeObject<List<Event>>(content);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(@"\tERROR {0}", ex.Message);
+        }
+
+        return Items;
+    }
+	}
+}
+
